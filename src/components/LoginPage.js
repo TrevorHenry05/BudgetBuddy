2770e1de5604d88933508876f0a0a3b5b89e9634
TrevorHenry05 <trevorjhen03@gmail.com>
2024-02-15 15:40:49 -0800

    Prettier and made styling of container important so it stays max-width of 500px.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import './Style.css'; // Import CSS file for styling
-import logo from './img/logo.jpg';
+import React, { useState } from "react";
+import "./Style.css"; // Import CSS file for styling
+import logo from "./img/logo.jpg";
 import { Link, useNavigate } from "react-router-dom";
 
 function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate(); // Initialize useNavigate
 
   const handleSubmit = (event) => {
     event.preventDefault(); // Prevent default form submission behavior
 
     if (email === "test@gmail.com" && password === "pass123!@#") {
-      navigate('/dashboard'); // Replace '/dashboard' with the desired route
+      navigate("/dashboard"); // Replace '/dashboard' with the desired route
     } else {
-      window.alert('Incorrect email or password. Please try again.'); // Display alert
+      window.alert("Incorrect email or password. Please try again."); // Display alert
     }
-    
   };
 
   return (
@@ -26,25 +25,29 @@ function LoginPage() {
       <p>Please sign up or log in to access your account.</p>
       <div className="form-container">
         <form onSubmit={handleSubmit}>
-          <input 
-            type="email" 
-            name="email" 
-            placeholder="Email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            required 
-          /><br />
-          <input 
-            type="password" 
-            name="password" 
-            placeholder="Password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            required 
-          /><br />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <br />
           <input type="submit" value="Log In" />
         </form>
-        <p>Don't have an account? <Link to="signUp">Sign Up</Link></p>
+        <p>
+          Don't have an account? <Link to="signUp">Sign Up</Link>
+        </p>
       </div>
     </div>
   );
