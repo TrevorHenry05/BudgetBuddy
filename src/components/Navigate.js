295ed8b2e568185774836539ddd809e5db507f8e
Yasin <yasini@uw.edu>
2024-02-15 19:52:54 -0800

    removed button from navigate

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -8,13 +8,11 @@ function Navigate() {
             <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
                 <div className="container-fluid">
                     <Link to="/" className="navbar-brand">Budget Budy</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                
                     <div className="collapse navbar-collapse" id="navbarText">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                                <NavLink exact to="/" className="nav-link">Budget Management</NavLink>
+                                <NavLink exact to="/Budget" className="nav-link">Budget Management</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to="/ExpenseTracking" className="nav-link">Expense tracking</NavLink>
