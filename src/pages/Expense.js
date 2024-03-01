38297c6fc7a10271a995698e3d44a5a08da78c5b
The-TUSK <marrubio07@gmail.com>
2024-02-29 18:00:10 -0800

    fixing layout of expense page

--- src/pages/Expense.js
+++ src/pages/Expense.js
@@ -45,7 +45,9 @@ function Expense() {
       setOriginalExpense(response.data);
     };
 
-    fetchExpenseData();
+    if (expenseId && token) {
+      fetchExpenseData();
+    }
   }, [expenseId, token]);
 
   const handleEdit = (e) => {
@@ -69,11 +71,23 @@ function Expense() {
   const handleSubmit = async (event) => {
     event.pereventDefault();
     if (altered) {
-      const response = await axios.put(`${API_URL}/api/expenses/` + expenseId)
-        .set("Authorization", `Bearer ${token}`)
-        .send(expense);
+      const response = await axios.put(`${API_URL}/api/expenses/` + expenseId,
+        {
+          amount: expense.amount,
+          date: expense.date,
+          description: expense.description,
+          userId: expense.userId,
+          categoryId: expense.categoryId,
+          groupId: expense.groupId,
+          budgetId: expense.budgetId
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          }
+        });
       if (response.statusCode === 200) window.alert("Expense details updated");
-      
+
     } else {
       window.alert("No changes have been made!");
     }
@@ -83,49 +97,56 @@ function Expense() {
     const response = await axios.delete(`${API_URL}/api/expenses/` + expenseId)
       .set("Authorization", `Bearer ${token}`);
     if (response.statusCode === 200) window.alert("Expense deleted")
-    navigate("/budget/" + orginalExpense.budgetID);
+    navigate("/budget/" + orginalExpense.budgetId);
   }
 
   return (
-    <>
-      <div className="modal">
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Expense Details</h5>
-              <button type="button" className="btn btn-warning" onClick={handleEdit(!isEditable)}>Edit</button>
-            </div>
-            <div className="modal-body">
-              <div>
-                {/** Top Section */}
-                <div>
-                  <label htmlFor="amount" className="form-label" data-bs-placement="left">Amount</label>
-                  <label htmlFor="date" className="form-label" data-bs-placement="right">Date</label>
-                </div>
-                <div>
-                  <input type="number" className="form-control" data-bs-placement="left" name="amount" value={expense.amount} onChange={(e) => handleChange(e.target.value)} required />
-                  <input type="date" className="form-control" data-bs-placement="right" name="date" value={expense.date} onChange={(e) => handleChange(e.target.value)} required />
-                </div>
-
-                <div>
-                  <label htmlFor="amount" className="form-label" data-bs-placement="left">Amount</label>
-                </div>
-                <div>
-                  <input type="text" className="form-control" data-bs-placement="left" name="amount" value={expense.amount} onChange={(e) => handleChange(e.target.value)} required />
-                </div>
-
-                {/** Bottom Section */}
-
-              </div>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-primary" onClick={handleSubmit}>Save changes</button>
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleCancel}>Cancel</button>
-              <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={handleDelete}>Delete</button>
+    < >
+      <section style={{
+        backgroundColor: "white",
+        borderRadius: "0.25rem",
+        width: "90%",
+        margin: "auto",
+      }}>
+        <div className="mb-3 d-flex justify-content-between align-items-center">
+          <h5 className="modal-title">Expense Details</h5>
+          <button type="button" className="btn btn-warning" onClick={() => handleEdit(!isEditable)}>Edit</button>
+        </div >
+        <div>
+          {/** Top Section */}
+          <div className="mb-3 d-flex justify-content-between align-items-center">
+            <div></div>
+            <div>
+              <label htmlFor="date" className="form-label" data-bs-placement="right">Date</label>
+              <input type="date" className="form-control" data-bs-placement="right" name="date" id="date" value={expense.date} onChange={(e) => handleChange(e.target.value)} required />
             </div>
           </div>
+          <div>
+            <label htmlFor="amount" className="form-label" data-bs-placement="left">Amount</label>
+            <input type="number" className="form-control" data-bs-placement="left" name="amount" id="amount" value={expense.amount} onChange={(e) => handleChange(e.target.value)} required />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="description" className="form-label" data-bs-placement="left">Description</label>
+            <input type="text" className="form-control" data-bs-placement="left" name="description" id="description" value={expense.description} onChange={(e) => handleChange(e.target.value)} required />
+          </div>
+
+          {/** Bottom Section */}
+
         </div>
-      </div>
+        <div className="mb-3 d-flex justify-content-between align-items-center">
+          <div></div>
+          <div>{isEditable === true ? (
+            <div>
+              <button type="button" className="btn btn-primary" onClick={handleSubmit}>Save changes</button>
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleCancel}>Cancel</button>
+            </div>
+          ) : (
+            <div></div>
+          )}</div>
+        </div>
+        <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={handleDelete}>Delete</button>
+      </section>
     </>
   );
 }
